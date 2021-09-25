2e930bdccaf1bb5497413a4ea990a6ec23e47dce
Aaron Lee <aaronlab.net@gmail.com>
2021-09-26 01:45:29 +0900

    custom view

--- 7th/app/src/main/java/com/example/a7th/MainActivity.java
+++ 7th/app/src/main/java/com/example/a7th/MainActivity.java
@@ -2,10 +2,51 @@ package com.example.a7th;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+class MyView extends View {
+
+    int key;
+    String str;
+    int x, y;
+
+    public MyView(Context context) {
+        super(context);
+
+        setBackgroundColor(Color.YELLOW);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = (int) event.getX();
+        y = (int) event.getY();
+
+        invalidate();
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        @SuppressLint("DrawAllocation") Paint paint = new Paint();
+        paint.setTextSize(50);
+
+        canvas.drawCircle(x, y, 30, paint);
+        canvas.drawText(x + "," + y, x, y + 100, paint);
+
+        super.onDraw(canvas);
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +55,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        configureButton();
+        MyView view = new MyView(this);
+        setContentView(view);
+//        configureButton();
     }
 
     // Configure Button
