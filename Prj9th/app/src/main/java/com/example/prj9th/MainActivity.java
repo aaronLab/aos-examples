c07d940492cb737b8e427a7f8a2a2c924a573750
Aaron Lee <aaronlab.net@gmail.com>
2021-10-02 15:06:41 +0900

    dialog

--- Prj9th/app/src/main/java/com/example/prj9th/MainActivity.java
+++ Prj9th/app/src/main/java/com/example/prj9th/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.prj9th;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureButtonListener();
+    }
+
+    /*
+    Button Listener
+     */
+    private void configureButtonListener() {
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(view -> {
+            showDialog("Do you want to pay?", "Cancel", "Confirm");
+        });
+    }
+
+    /*
+    Show Dialog
+     */
+    private void showDialog(String title,
+                            String cancelTitle,
+                            String confirmTitle) {
+        // Builder
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setTitle(title);
+
+        // Confirm
+        alertBuilder.setPositiveButton(confirmTitle, (dialogInterface, i) -> {
+            showShortToast("Done!");
+        });
+
+        // Cancel
+        alertBuilder.setNegativeButton(cancelTitle, (dialogInterface, i) -> {
+            showShortToast("Canceled!");
+        });
+
+        // Show
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
     }
 
     /*
@@ -34,31 +72,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    Show a Toast message
+    Show Short Toast
      */
-    private void showShortToast(MenuOption menu) {
-        String message = String.format("%s pressed.", menu.label);
+    private void showShortToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+    Show a fruit Toast message
+     */
+    private void showFruitShortToast(MenuOption menu) {
+        String message = String.format("%s pressed.", menu.label);
+        showShortToast(message);
     }
 
     /*
     When Apple menu pressed
      */
     public void applePressed(MenuItem item) {
-        showShortToast(MenuOption.APPLE);
+        showFruitShortToast(MenuOption.APPLE);
     }
 
     /*
     When Grape menu pressed
      */
     public void grapePressed(MenuItem item) {
-        showShortToast(MenuOption.GRAPE);
+        showFruitShortToast(MenuOption.GRAPE);
     }
 
     /*
     When Banana menu pressed
      */
     public void bananaPressed(MenuItem item) {
-        showShortToast(MenuOption.BANANA);
+        showFruitShortToast(MenuOption.BANANA);
     }
 }
